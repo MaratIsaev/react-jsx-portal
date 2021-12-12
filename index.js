95d465d1af4acc44742c8a3525c4ba2deac12e4f
Marat Isaev <m.isaev@city-mobil.ru>
2021-12-12 12:18:53 +0300

    send Anchor props to Portal

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-import React, {useLayoutEffect, useEffect, useRef, useState} from "react";
+import React, {useLayoutEffect, useEffect, useRef, useState, useCallback} from "react";
 import ReactDOM from "react-dom";
 
 const DID_MOUNT_CUSTOM_EVENT = 'didMountCustomEvent'
 const DID_UNMOUNT_CUSTOM_EVENT = 'didUnmountCustomEvent'
+const ANCHOR_PROPS_DID_UPDATE_EVENT = 'anchorPropsDidUpdateEvent'
+const PORTAL_MOUNTED_AND_NEEDS_ANCHOR_PROPS_EVENT = 'portalMounetAndNeedsAnchorEvent'
 
 const storeElems = {}
 
@@ -65,22 +67,33 @@ const unregisterPortal = (id) => {
 }
 
 export const Portal = (props) => {
-    const { children, id } = props
+    const { render, id } = props
     const elem = useRef(requireElem(id))
     const [mounted, setMounted] = useState(storeAnchors[id])
+    const [anchorProps, setAnchorProps] = useState()
 
     useLayoutEffect(() => {
         const handleMount = () => setMounted(true)
-        const handleUnmount = () => setMounted(false)
+        const handleUnmount = () => {
+            setMounted(false)
+            setAnchorProps()
+        }
+        const handleAnchorPropsUpdate = (e) => setAnchorProps(e.detail)
 
         elem.current.addEventListener(DID_MOUNT_CUSTOM_EVENT, handleMount)
         elem.current.addEventListener(DID_UNMOUNT_CUSTOM_EVENT, handleUnmount)
+        elem.current.addEventListener(ANCHOR_PROPS_DID_UPDATE_EVENT, handleAnchorPropsUpdate)
+
+        const portalMounetAndNeedsAnchor = new CustomEvent(PORTAL_MOUNTED_AND_NEEDS_ANCHOR_PROPS_EVENT)
+
+        elem.current.dispatchEvent(portalMounetAndNeedsAnchor)
 
         registerPortal(id)
 
         return () => {
             elem.current.removeEventListener(DID_MOUNT_CUSTOM_EVENT, handleMount)
             elem.current.removeEventListener(DID_UNMOUNT_CUSTOM_EVENT, handleUnmount)
+            elem.current.removeEventListener(ANCHOR_PROPS_DID_UPDATE_EVENT, handleAnchorPropsUpdate)
 
             unregisterPortal(id)
 
@@ -88,15 +101,29 @@ export const Portal = (props) => {
         }
     }, [])
 
-    return mounted ? ReactDOM.createPortal(children, elem.current) : null
+    return mounted && anchorProps ? ReactDOM.createPortal(render(anchorProps), elem.current) : null
 }
 
 export const Anchor = (props) => {
     const { id, ...restProps } = props
     const ref = useRef(null)
     const elem = useRef(requireElem(id))
+    const refProps = useRef(props)
+    refProps.current = props
+
+    const sendAnchorPropsToPortal = useCallback(() => {
+        const anchorPropsDidUpdateEvent = new CustomEvent(ANCHOR_PROPS_DID_UPDATE_EVENT, { detail: refProps.current })
+
+        elem.current.dispatchEvent(anchorPropsDidUpdateEvent)
+    }, [])
 
     useEffect(() => {
+        sendAnchorPropsToPortal()
+    }, [props, sendAnchorPropsToPortal])
+
+    useEffect(() => {
+        elem.current.addEventListener(PORTAL_MOUNTED_AND_NEEDS_ANCHOR_PROPS_EVENT, sendAnchorPropsToPortal)
+
         ref.current.appendChild(elem.current)
 
         const customDidMountEvent = new CustomEvent(DID_MOUNT_CUSTOM_EVENT)
@@ -106,6 +133,8 @@ export const Anchor = (props) => {
         registerAnchor(id)
 
         return () => {
+            elem.current.removeEventListener(PORTAL_MOUNTED_AND_NEEDS_ANCHOR_PROPS_EVENT, sendAnchorPropsToPortal)
+
             const customDidUnmountEvent = new CustomEvent(DID_UNMOUNT_CUSTOM_EVENT)
 
             elem.current.dispatchEvent(customDidUnmountEvent)
