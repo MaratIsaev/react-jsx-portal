2e7cf724f7283ea1d69b0ce86a5e2881b92ff2e3
Marat Isaev <m.isaev@city-mobil.ru>
2021-12-15 16:50:46 +0300

    webpack build

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require('path');
+
+module.exports = {
+    output: {
+        clean: true,
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'index.js',
+        library: {
+            type: 'umd',
+        },
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: ['babel-loader'],
+            },
+        ],
+    },
+    externals: {
+        react: {
+            root: 'React',
+            commonjs2: 'react',
+            commonjs: 'react',
+            amd: 'react'
+        },
+        'react-dom': {
+            root: 'ReactDOM',
+            commonjs2: 'react-dom',
+            commonjs: 'react-dom',
+            amd: 'react-dom'
+        },
+    },
+    optimization: {
+        usedExports: false,
+    },
+}
