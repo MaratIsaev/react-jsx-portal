669231aaf1ee17d93e0ca7d6de72f4432326afe9
Marat Isaev <m.isaev@city-mobil.ru>
2021-12-28 17:57:23 +0300

    refactor tests

--- test/index.test.js
+++ test/index.test.js
@@ -21,9 +21,9 @@ afterEach(() => {
   })
 })
 
-test('Anchor и Portal появляются одновременно, но Anchor выше в дереве', () => {
+const testAnchorAndPortl = (isAnchorFirst) => {
   act(() => {
-    render(<App id={ID} anchorVisible portalVisible initCount={INIT_COUNT} anchorFirst />, root)
+    render(<App id={ID} anchorVisible portalVisible initCount={INIT_COUNT} anchorFirst={isAnchorFirst} />, root)
   })
 
   //Проверяем, что портал появился
@@ -101,89 +101,11 @@ test('Anchor и Portal появляются одновременно, но Ancho
   increase.click()
 
   expect(count.innerHTML).toBe(String(INIT_COUNT))
-})
+}
 
-test('Anchor и Portal появляются одновременно, но Portal выше в дереве', () => {
-  act(() => {
-    render(<App id={ID} anchorVisible portalVisible initCount={INIT_COUNT} />, root)
-  })
+test('Anchor и Portal появляются одновременно, но Anchor выше в дереве', () => testAnchorAndPortl(true))
 
-  //Проверяем, что портал появился
-  let portal = document.querySelector('#counter')
-
-  expect(portal).toBe
-
-  //Проверяем, что значение count равно начальному
-  let count = document.querySelector('#count')
-
-  expect(count.innerHTML).toBe(String(INIT_COUNT))
-
-  //Проверяем что пропсы нормально передались в Portal
-  //Для этого сначала увеличим счетчик
-  let increase = document.querySelector('#increase')
-
-  increase.click()
-
-  expect(count.innerHTML).toBe(String(INIT_COUNT + 1))
-
-  //Потом уменьшим
-  const decrease = document.querySelector('#decrease')
-
-  decrease.click()
-  decrease.click()
-
-  expect(count.innerHTML).toBe(String(INIT_COUNT - 1))
-
-  //Скрыть Anchor проверить что Portal пропал
-  const close = document.querySelector('#close')
-
-  close.click()
-
-  portal = document.querySelector('#counter')
-
-  expect(portal).not.toBe
-
-  //Показать Anchor проверить, что Portal появился
-  const openAnchor = document.querySelector('#openAnchor')
-
-  openAnchor.click()
-
-  portal = document.querySelector('#counter')
-
-  expect(portal).toBe
-
-  count = document.querySelector('#count')
-
-  expect(count.innerHTML).toBe(String(INIT_COUNT - 1))
-
-  //Прячем Portal
-  const closePortal = document.querySelector('#closePortal')
-
-  closePortal.click()
-
-  portal = document.querySelector('#counter')
-
-  expect(portal).not.toBe
-
-  //Показываем Portal
-  const openPortal = document.querySelector('#openPortal')
-
-  openPortal.click()
-
-  portal = document.querySelector('#counter')
-
-  expect(portal).toBe
-
-  count = document.querySelector('#count')
-
-  expect(count.innerHTML).toBe(String(INIT_COUNT - 1))
-
-  increase = document.querySelector('#increase')
-
-  increase.click()
-
-  expect(count.innerHTML).toBe(String(INIT_COUNT))
-})
+test('Anchor и Portal появляются одновременно, но Portal выше в дереве', () => testAnchorAndPortl(false))
 
 test('Проверяем что сторы очищаются корректно и нет утечек памяти', () => {
   const {
